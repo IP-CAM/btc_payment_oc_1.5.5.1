87a1fcaa84eb97eb87d085ffc85a037663700069
Pavlo K <virgeny@gmail.com>
2019-02-02 23:39:17 +0200

    english transaction for client side

---
+++ upload/catalog/language/english/payment/btc_payment.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_title']       = 'Bitcoin transfer';
+$_['text_instruction'] = 'Payment instructions';
+$_['text_description'] = 'Please, send <b>%s</b> BTC to wallet:';
+$_['text_payment']     = 'After payment is done - click "Confirm order". <br> We will not process the order until the transaction confirmmation in blockchain. If after 6 hours after receiving order, transaction will not be confirmed - the store may cancel the order and return funds.';
+$_['text_time_request']     = 'Invoice valid for 15 minutes. Time left: ';
+$_['text_timeleft']     = 'Time left, please refresh page';
+$_['text_paymenttime']     = 'unix time of invoice generation: <a href="https://www.epochconverter.com/timezones?q=%s">%s</a>';
+?>
